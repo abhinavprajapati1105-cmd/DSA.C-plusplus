81f2b453ee759fb865dab9b7b28058c74991626c
Abhinav Prajapati <your-email@example.com>
2026-07-28 17:39:25 +0530

    Added new C++ programs

--- greatestIn3.cpp
+++ greatestIn3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int a,b,c;
     cout<<"Enter the value of a,b and c:";
-    cin>>a,b,c;
+    cin>>a>>b>>c;
     if((a>b) && (a>c)){
         cout<<"a="<<a;
     }else if((b>a) && (b>c)){
